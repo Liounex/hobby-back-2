b71b98a406be3f343986cc77be3e52ff08722563
Jhimmy <pbbanger32@gmail.com>
2023-10-20 09:03:52 -0500

    sdfsd

--- app/api.py
+++ app/api.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get('/', tags=["home"])
 async def hello():
     return {"message": "Hello World"}
+
+
+@app.get('/api', tags=["data"])
+async def prueba():
+    return {"message": "Hello World"}
